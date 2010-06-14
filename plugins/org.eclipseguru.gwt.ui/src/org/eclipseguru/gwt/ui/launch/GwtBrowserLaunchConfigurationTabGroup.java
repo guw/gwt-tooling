9f9131a0ff3dee1f94e32c72f03cb104da1d5ceb
eclipseguru <eclipseguru@191dd8b7-711b-0410-acaf-1d86e3772e8e>
2010-06-14 19:52:56 +0000

    - fixed an issue with preferences - restored browser launch config to allow dev mode launch

---
+++ plugins/org.eclipseguru.gwt.ui/src/org/eclipseguru/gwt/ui/launch/GwtBrowserLaunchConfigurationTabGroup.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2008 EclipseGuru and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     EclipseGuru - initial API and implementation
+ *******************************************************************************/
+package org.eclipseguru.gwt.ui.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
+
+/**
+ * Tab group for the GWT hosted mode server.
+ */
+public class GwtBrowserLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(final ILaunchConfigurationDialog dialog, final String mode) {
+		setTabs(new ILaunchConfigurationTab[] { new GwtMainTab(), new GwtShellArgumentsTab(), new JavaJRETab(), new CommonTab() });
+	}
+
+}
